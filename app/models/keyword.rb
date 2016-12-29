74af36cd6e02ca149680ec2edcfd6b238a1b2a2b
Tunde Idahor <bidahor13@wou.edu>
2016-12-28 16:30:11 -0800

    Twitter API added to twitter Gem added to App

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -1,2 +1,15 @@
 class Keyword < ActiveRecord::Base
-end
+  def grab_twitts
+    client = Twitter::REST::Client.new do |config|
+     config.consumer_key        = "3YO3Jm1Zt3qjHNUWAuetu9E2c"
+     config.consumer_secret     = "wck9VN4NhaujZMEUbPWsL2Yb5QnlsGl0PrAXfwcenIE8acwXs7"
+     config.access_token        = "597532306-APsZQIfvEvhVB3WS1YF6rTq6qZQbh4W3m1HF1UNO"
+     config.access_token_secret = "F8Zt062UdUCKjDKQKtbUF2BIBVtQZxKyuccXqf5HVoZrl"
+    end
+
+    client.search(self.word, result_type: "recent").take(3).collect do |tweet|
+     "#{tweet.user.screen_name}: #{tweet.text}"
+    end
+
+  end
+ end
