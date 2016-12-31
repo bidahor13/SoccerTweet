8ae63e4b623dbffe1847d0c3647cd6e421eed23e
Tunde Idahor <bidahor13@wou.edu>
2016-12-31 00:23:33 -0800

    new model and migrations

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -1,4 +1,6 @@
 class Keyword < ActiveRecord::Base
+  has_many :tweets
+
   def grab_twitts
     client = Twitter::REST::Client.new do |config|
      config.consumer_key        = "3YO3Jm1Zt3qjHNUWAuetu9E2c"
@@ -7,9 +9,21 @@ class Keyword < ActiveRecord::Base
      config.access_token_secret = "F8Zt062UdUCKjDKQKtbUF2BIBVtQZxKyuccXqf5HVoZrl"
     end
 
-    client.search(self.word, result_type: "recent").take(5).collect do |tweet|
+    client.search(self.word,  result_type: "recent").take(10).collect do |tweet|
+      new_tweet = Tweet.new
+      new_tweet.tweet_id=tweet.id.to_s
+      new_tweet.tweet_created_at=tweet.created_at
+      new_tweet.text = tweet.text
+      new_tweet.user_uid= tweet.user.id
+      new_tweet.user_name=tweet.user.name
+      new_tweet.user_screen_name=tweet.user.screen_name
+      new_tweet.user_image_url=tweet.user.profile_image_url
+
+      new_tweet.keyword=self
+
+      new_tweet.save
+
      "#{tweet.user.screen_name}: #{tweet.text}"
     end
-
   end
  end
